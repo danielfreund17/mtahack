e2cf433abc4d6fde6615c61c0e987c7aa5b29b88
idandagan1 <idandagan1@gmail.com>
2017-05-30 21:58:25 +0300

    Adding server property

--- client/view/app/app.component.ts
+++ client/view/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'components works!';
+export class AppComponent implements OnInit {
+  server: string;
+
+  ngOnInit() {
+    this.server = 'new server';
+  }
 }
