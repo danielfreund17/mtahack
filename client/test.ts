0d50bae460f5061b78ff0aeae1a46d93ed79c257
idandagan1 <idandagan1@gmail.com>
2017-05-30 22:10:11 +0300

    Adding server property

--- client/test.ts
+++ client/test.ts
@@ -30,3 +30,70 @@ const context = require.context('./', true, /\.spec\.ts$/);
 context.keys().map(context);
 // Finally, start Karma to run the tests.
 __karma__.start();
+
+const router;
+const currentUser;
+const Users;
+const Item;
+
+router.get('/getData', (req,res) => {
+
+  if(!currentUser){//User is not logged in
+    return res.status(404).send("UserName");
+  }
+
+  Users.findOne({UserName: currentUser})
+    .then( doc => {
+      const items = doc.Items;
+      const listOfItems = [];
+      items.forEach( item => {
+        listOfItems.push(item);
+      })
+      res.send({list: listOfItems});
+    });
+})
+
+router.delete('/deleteItem', (req, res) => {
+
+  if (!currentUser) {
+    return res.status(404).send('UserName');
+  }
+  const itemToDelete = req.body;
+
+  Users.findOneAndUpdate(
+    {UserName: currentUser},
+    {$pull:{Items: {Name: itemToDelete.Name}}},
+    (err, data) => {
+      if(err) {
+        console.log("error");
+        return res.status(500).json({'error' : 'error in deleting address'});
+      }else{
+        console.log("Deleted "+req.body.Name+" from list.");
+        res.end('{"success" : "Delete item Successfully", "status" : 200}');
+      }
+    }
+  );
+})
+router.post('/addItem', (req, res) => {
+
+  const item = req.body;
+
+  if (!currentUser) {
+
+    Users.findById(currentUser, (err, result) =>{
+      if (result) {
+        const newItem = new Item(item);
+        const newitem = {
+          _id: newItem._id,
+          name: item.Name
+        }
+
+        result.Items.push(newitem);
+        result.save();
+        res.end('{"success" : "Added Item Successfully", "status" : 200}');
+      }
+
+    });
+  }
+
+});
