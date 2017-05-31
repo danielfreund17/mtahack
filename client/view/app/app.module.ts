03caafd260b2405a046a70e4ad7f7fdc99f660e6
idandagan1 <idandagan1@gmail.com>
2017-05-31 12:25:39 +0300

    Added Footer and implemented routing

--- client/view/app/app.module.ts
+++ client/view/app/app.module.ts
@@ -2,22 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from '../../components/header/header.component';
+import { HeaderComponent, FooterComponent } from '../../components';
 import { DashboardComponent, ContactComponent } from '../index';
 
+
+const appRoutes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'contact', component: ContactComponent }
+];
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     ContactComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
